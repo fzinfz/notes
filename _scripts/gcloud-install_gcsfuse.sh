8a953ffffab8b0968a8c238306e1745b531519b8
fzinfz <fzinfz@gmail.com>
2017-07-01 00:43:07 +0800

    Update gcloud-install_gcsfuse.sh

--- _scripts/gcloud-install_gcsfuse.sh
+++ _scripts/gcloud-install_gcsfuse.sh
@@ -1,1 +1,5 @@
-export GCSFUSE_REPO=gcsfuse-zesty && gcloud-install_gcsfuse.sh echo "deb http://packages.cloud.google.com/apt gcsfuse-zesty main" |  tee /etc/apt/sources.list.d/gcsfuse.list && gcloud-install_gcsfuse.sh curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add - && gcloud-install_gcsfuse.sh apt-get update && gcloud-install_gcsfuse.sh apt-get install -y gcsfuse EOL
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s` && \
+	echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" |  tee /etc/apt/sources.list.d/gcsfuse.list && \
+	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add - && \
+	apt-get update && \
+	apt-get install -y gcsfuse
