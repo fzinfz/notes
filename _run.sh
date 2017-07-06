809be2810cc63df90aa890fdc66678d246491c05
fzinfz <fzinfz@gmail.com>
2017-07-06 20:55:02 +0800

    Update _run.sh

--- _run.sh
+++ _run.sh
@@ -2,12 +2,13 @@ p=$1
 shift
 
 case $p in 
+	update ) 
+		cmd="wget -N https://raw.githubusercontent.com/fzinfz/notes/master/run.sh"
+		;;
 	nmap )
 		cmd="$p -sV -p$2 $1"
 		;;
-	* )
-		cmd="$p $*"
-		;;
+	*)
 esac
 
 echo $cmd
