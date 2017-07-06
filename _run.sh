ddac9dcda45f44fb1bd036f195cef978e82ed7ab
Ferro Fang <fzinfz@gmail.com>
2017-07-06 20:55:44 +0800

    update

--- _run.sh
+++ _run.sh
@@ -3,7 +3,7 @@ shift
 
 case $p in 
 	update ) 
-		cmd="wget -N https://raw.githubusercontent.com/fzinfz/notes/master/run.sh"
+		cmd="wget -N https://raw.githubusercontent.com/fzinfz/notes/master/_run.sh"
 		;;
 	nmap )
 		cmd="$p -sV -p$2 $1"
