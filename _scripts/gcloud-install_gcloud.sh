f72a858553e0e1318d6f988234cef906222a9b63
Ferro Fang <fzinfz@gmail.com>
2017-08-05 01:12:48 +0800

    update

--- _scripts/gcloud-install_gcloud.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-apt install -y python-dev || yum install -y python-dev
-
-curl https://sdk.cloud.google.com | bash
-export PATH="/root/google-cloud-sdk/bin/:$PATH"
-
-gcloud auth application-default login
